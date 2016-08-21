0e0205524540ca63c6bc9b798ab9d5133e9482b6
mrcharif <mrcharifmakaoui@gmail.com>
2016-08-21 09:41:30 +0100

    make it better

--- app_boot.sh
+++ app_boot.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Artisan commands
-/app/.heroku/php/bin/php /app/artisan clear-compiled
-/app/.heroku/php/bin/php /app/artisan optimize
-
-
 # Boot up!
 vendor/bin/heroku-php-apache2 public/
